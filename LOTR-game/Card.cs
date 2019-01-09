1b80f50d812ae37fb6acf97b90c9201135f603ea
erremolerup <erika.molinder@gmail.com>
2019-01-09 13:17:04 +0100

    First

---
+++ LOTR-game/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOTR_game
+{
+    public class Card
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public CardType Type { get; set; }
+        public CardAbility Ability { get; set; }
+        public int Attack { get; set; }
+        public int Health { get; set; }
+    }
+
+    public enum CardType
+    {
+        Creature,
+        Spell
+    }
+}
---
+++ LOTR-game/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOTR_game
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int LifePoints { get; set; }
+        public bool ActivePlayer { get; set; }
+    }
+}
